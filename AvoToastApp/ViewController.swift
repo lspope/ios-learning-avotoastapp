4b90302c5a5f4884409d8d8d5a2ffd99d15d34d2
Leah Pope <name@email.com>
2020-06-12 18:02:49 -0500

    Initial Commit

---
+++ AvoToastApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AvoToastApp
+//
+//  Created by Leah Pope on 4/18/20.
+//  Copyright © 2020 lp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
